169ab1623435b9915e0c8fd242cc15bad476bb4e
Benjamín Canales Cempe <135075287+benjaminCanalesC@users.noreply.github.com>
2023-11-17 21:45:38 -0300

    feat: postgis update

--- dbLoad.sql
+++ dbLoad.sql
@@ -72,25 +72,38 @@ VALUES
 		
 	(6, 'Institución de Prevención de Derrumbes y Deslizamiento de Tierra (IPDDT)',
 	 'es una entidad especializada en la prevención y mitigación de derrumbes y deslizamientos de tierra en áreas montañosas y colinas. Implementan sistemas de monitoreo de la estabilidad del suelo y colaboran con las comunidades en la planificación de medidas preventivas');
-		 
-INSERT INTO emergency (id_emergency, emergency_name, emergency_location, 
-					   emergency_type, statement_date, id_state)
+		
+INSERT INTO emergency_address (id_address_e, address, longitude, latitude) VALUES
+	(1, 'Lautaro, Araucanía', -38.57434144039071, -72.4756987329032),
+  	(2, 'Viña del Mar, Valparaíso', -32.993457378637764, -71.53361941561872),
+  	(3, 'Pucón, Araucanía', -39.41955618986563, -71.93942618853083),
+  	(4, 'Lago Ranco, Los Ríos', -40.57747844827906, -72.10983024396644),
+  	(5, 'Mall Zofri - Sector antiguo, modulo 130, Iquique, Tarapacá', -20.22959943149161, -70.13553539158428),
+  	(6, 'Sucre 1641, 1340517 Tocopilla, Antofagasta', -22.093843895975603, -70.20005600801197),
+  	(7, 'Padre Felipe Gómez de Vidaurre, 8330369 Santiago, Región Metropolitana', -33.447969674529496, -70.65607952327353),
+  	(8, 'Candelaria Pérez 26-66, Illapel, Coquimbo', -31.631798107170333, -71.17459210501087),
+  	(9, 'Av. Angamos 290-194, Antofagasta', -23.67586252273854, -70.40806255684329),
+  	(10, 'Constitución, Maule', -35.33074968305388, -72.43287959887027),
+  	(11, 'Villa Santa Lucía, Chaitén, Los Lagos', -43.4106060512741, -72.36752613467937),
+  	(12, 'Los Angeles, Bío Bío', -37.42699254228149, -72.3287247972075);	
+
+UPDATE emergency_address SET geom = ST_SetSRID(ST_MakePoint(latitude, longitude), 4326);
+		
+INSERT INTO emergency (id_emergency, emergency_name, 
+					   emergency_type, statement_date, id_state, id_address_e)
 VALUES
-	(1, 'Incendio forestal en comuna Lautaro', 'Lautaro, Región de la Araucanía',
-	'0', '2023-10-15', 1),
-	(2, 'Incendio forestal en Viña del Mar', 'Viña del Mar, Región de Valparaíso',
-	'0', '2023-10-14', 1),
-	(3, 'Erupción Volcán Villarica', 'Región de la Araucanía', '1', '2023-10-16', 1),
-	(4, 'Erupción complejo volcánico Puyehue', 'Región de los Ríos', '1','2023-10-12', 1),
-	(5, 'Terremoto de Iquique', 'Región de Tarapacá', '2', '2023-10-15', 1),
-	(6, 'Terremoto Tocopilla', 'Región de Antofagasta', '2', '2023-10-13', 1),
-	(7, 'Derrumbe Santiago', 'Región Metropolitana', '3', '2023-10-09', 1),
-	(8, 'Derrumbes en Illapel', 'Región de Coquimbo', '3', '2023-10-10', 1),
-	(9, 'Tsunami en Antofagasta', 'Región de Antofagasta', '4', '2023-10-08', 1),
-	(10, 'Tsunami en Constitución', 'Región de El Maule', '4', '2023-10-08', 1),
-	(11, 'Aluvión de Villa Santa Lucía', 'Chaitén, Región de Los Lagos', 
-	 '5', '2023-10-13', 1),
-	(12, 'Aluvión del Biobío', 'Región del Biobío', '5', '2023-10-14', 1);
+	(1, 'Incendio forestal en comuna Lautaro', '0', '2023-10-15', 1, 1),
+	(2, 'Incendio forestal en Viña del Mar', '0', '2023-10-14', 1, 2),
+	(3, 'Erupción Volcán Villarica', '1', '2023-10-16', 1, 3),
+	(4, 'Erupción complejo volcánico Puyehue', '1','2023-10-12', 1, 4),
+	(5, 'Terremoto de Iquique', '2', '2023-10-15', 1, 5),
+	(6, 'Terremoto Tocopilla', '2', '2023-10-13', 1, 6),
+	(7, 'Derrumbe Santiago', '3', '2023-10-09', 1, 7),
+	(8, 'Derrumbes en Illapel', '3', '2023-10-10', 1, 8),
+	(9, 'Tsunami en Antofagasta', '4', '2023-10-08', 1, 9),
+	(10, 'Tsunami en Constitución', '4', '2023-10-08', 1, 10),
+	(11, 'Aluvión de Villa Santa Lucía', '5', '2023-10-13', 1, 11),
+	(12, 'Aluvión del Biobío', '5', '2023-10-14', 1, 12);
 
 INSERT INTO emergency_institution (id_emergency, id_institution)
 VALUES
@@ -153,33 +166,61 @@ VALUES
     (23, 'enlace', '45.567.890-K', 'Andrés', 'Manuel', 'Sanchez', 'Silva', 'Coordinador de Voluntarios en CAV', 'M', '1987-06-30'),
     (24, 'enlace', '56.678.901-3', 'Mariana', 'Javiera', 'Vargas', 'Muñoz', 'Coordinadora de Respuesta a Tsunamis en IDT', 'F', '1992-01-25');
 
+INSERT INTO user_address (id_address_u, address, latitude, longitude) VALUES
+  	(1, 'Caupolicán 1027, Concepción, Bío Bío', -73.0547538041757, -36.821393959657435),
+  	(2,'Avda. Fco de Aguirre 170, 1710156 La Serena, Coquimbo', -71.25461346356609, -29.90545603158812),
+  	(3, 'Av. Carlos Condell 1127, 2261467 Quillota, Valparaíso', -71.24314311789601, -32.89143880250012),
+  	(4, 'C. Nizar 8809, San Bernardo, Región Metropolitana', -70.74877907552514, -33.66914744049816),
+  	(5, 'Dr. Manuel Barros Borgoño 340, Providencia, Región Metropolitana', -70.61747977739354, -33.431580375509526),
+  	(6, 'Gral. Pedro Lagos Marchant 1110, 1000187 Arica, Arica y Parinacota', -70.31187746273224, -18.475727169073984),
+  	(7, 'Independencia 640, 1000099 Arica, Arica y Parinacota', -70.31045352040502, -18.47447196422832),
+  	(8, 'Cerro Moreno 10500, 1262357 Antofagasta', -70.3926797337, -23.571606640754734),
+  	(9, 'Conferencia 1076, 8370569 Santiago, Región Metropolitana', -70.67478893135461, -33.462484966475095),
+  	(10,'Manuel de Amat 3138, 8370750 Santiago, Región Metropolitana',-70.67768678902719, -33.46849237489128),
+  	(11, 'El Valle 1294, San Bernardo, Región Metropolitana', -70.70928809087626, -33.572705313299025),
+  	(12, 'Maipú 1843, 4081865 Concepción, Bío Bío', -73.03691617719457, -36.81804455010238),
+  	(13, '10902 Los Almendros La Pintana, Región Metropolitana', -70.64430219087696, -33.56058374876691),
+  	(14, 'Pje. Benozzo Gozzoli 4339, San Joaquín, Región Metropolitana', -70.63028937553493, -33.496725522081356),
+  	(15 ,'Luis Beltrán 731, 9020841 Santiago, Pudahuel, Región Metropolitana', -70.75959567739277, -33.445750342297146),
+  	(16 ,'Mujica 301, 9200800 Cerrillos, Región Metropolitana', -70.71825279088014, -33.50358528718388),
+  	(17 ,'Federico Gana 3575, Macul, Región Metropolitana', -70.5951228890261, -33.48790802037042),
+  	(18 ,'El Mesías 3948, 1731947 La Serena, Coquimbo', -71.22865457573029, -29.88598263106388),
+  	(19 ,'Pje. Vallenar 2512, 1732932 La Serena, Coquimbo', -71.22717998922131, -29.879434620932617),
+  	(20 ,'Joaquín Prieto 1131, 4081368 Concepción, Bío Bío', -73.04981053301252,-36.815639021789565),
+  	(21 ,'Pje. Cinco 134, 4060831 Concepción, Bío Bío', -73.07152908883074, -36.81001301524118),
+  	(22 ,'José Domingo Cañas 510, 7750078 Ñuñoa, Región Metropolitana', -70.6245400178644, -33.45482036051855),
+  	(23 ,'Caupolicán 660, Providencia, Región Metropolitana', -70.62165556569133, -33.44738511181257),
+  	(24 ,'Pje. 11 de Septiembre 230, Mejillones, Antofagasta', -70.44292641837411, -23.099618075044457);
+
+UPDATE user_address SET geom = ST_SetSRID(ST_MakePoint(latitude, longitude), 4326);
+  
 INSERT INTO "user" (id_user, username, password, id_profile,
-					 id_role, id_institution)
+					 id_role, id_institution, id_address_u)
 VALUES
-	(1, 'aracelyC', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 1, 1, 1),
-	(2, 'mieres7', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 2, 1, 2),
-	(3, 'garciaB', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 3, 1, 3),
-	(4, 'benjaminC', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 4, 1, 4),
-	(5, 'daniE', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 5, 1, 5),
-	(6, 'humbertoS', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 6, 2, 2),
-	(7, 'muñozC', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 7, 2, 4),
-	(8, 'sofiCa', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 8, 2, 5),
-	(9, 'fernandoR', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 9, 2, 1), 
-    (10, 'luciaT', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 10, 2, 2),  
-    (11, 'raulM', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 11, 2, 3),  
-    (12, 'martaG', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 12, 3, 4),  
-    (13, 'manuelH', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 13, 3, 5),  
-    (14, 'camilaS', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 14, 2, 6),
-	(15, 'luisP', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 15, 3, 1),  
-    (16, 'anaR', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 16, 3, 1),  
-    (17, 'diegoO', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 17, 3, 2),  
-    (18, 'elenaL', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 18, 3, 3),  
-    (19, 'joseM', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 19, 3, 4),  
-    (20, 'valentinaG', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 20, 3, 5),  
-    (21, 'federicoD', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 21, 3, 6),  
-    (22, 'lauraC', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 22, 3, 1),  
-    (23, 'andresS', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 23, 3, 2),  
-    (24, 'marianaV', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 24, 3, 3);
+	(1, 'aracelyC', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 1, 1, 1, 1),
+	(2, 'mieres7', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 2, 1, 2, 2),
+	(3, 'garciaB', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 3, 1, 3, 3),
+	(4, 'benjaminC', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 4, 1, 4, 4),
+	(5, 'daniE', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 5, 1, 5, 5),
+	(6, 'humbertoS', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 6, 2, 2, 6),
+	(7, 'muñozC', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 7, 2, 4, 7),
+	(8, 'sofiCa', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 8, 2, 5, 8),
+	(9, 'fernandoR', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 9, 2, 1, 9), 
+    (10, 'luciaT', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 10, 2, 2, 10),  
+    (11, 'raulM', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 11, 2, 3, 11),  
+    (12, 'martaG', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 12, 3, 4, 12),  
+    (13, 'manuelH', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 13, 3, 5, 13),  
+    (14, 'camilaS', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 14, 2, 6, 14),
+	(15, 'luisP', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 15, 3, 1, 15),  
+    (16, 'anaR', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 16, 3, 1, 16),  
+    (17, 'diegoO', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 17, 3, 2, 17),  
+    (18, 'elenaL', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 18, 3, 3, 18),  
+    (19, 'joseM', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 19, 3, 4, 19),  
+    (20, 'valentinaG', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 20, 3, 5, 20),  
+    (21, 'federicoD', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 21, 3, 6, 21),  
+    (22, 'lauraC', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 22, 3, 1, 22),  
+    (23, 'andresS', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 23, 3, 2, 23),  
+    (24, 'marianaV', '$2a$10$klotFGdYLOB1mPcEzpoqLOIiqOdrs72Pj/Cs.RgOd8Hp4OMWDOj42', 24, 3, 3, 24);
 
 INSERT INTO user_ability (id_user, id_ability)
 VALUES
