651d9159dd0476ac3a3fd0deb1bf44d8f9bb1582
AracelyU <aracely.castro@usach.cl>
2023-12-16 18:16:06 -0300

    Update TaskMongoRepositoryImg.java

--- src/main/java/grupo3/backend/repositoriesMongo/TaskMongoRepositoryImg.java
+++ src/main/java/grupo3/backend/repositoriesMongo/TaskMongoRepositoryImg.java
@@ -19,6 +19,29 @@ public class TaskMongoRepositoryImg implements TaskMongoRepository {
     @Autowired
     MongoDatabase database;
 
+
+    /*
+    La función getActiveTasksForEmergency del repositorio TaskMongoRepositoryImg en el código que proporcionaste se utiliza para obtener una lista de tareas activas asociadas a una emergencia específica en una base de datos MongoDB. He aquí un desglose de lo que hace:
+
+1. Dependencias:
+
+Importa las clases necesarias para conectarse a la base de datos MongoDB, trabajar con documentos y colecciones, y mapear entidades a clases POJO.
+Inyecta la base de datos MongoDB database usando @Autowired.
+2. Método getActiveTasksForEmergency:
+
+Toma la emergencyId como parámetro.
+Obtiene las colecciones task y emergencyTask de la base de datos.
+Crea una lista de documentos pipeline para definir la agregación:
+Etapa de match: Filtra los documentos de emergencyTask por id_emergency igual al emergencyId proporcionado.
+Etapa de lookup: Realiza una unión entre las colecciones emergencyTask y task en base a los campos id_task (campo común). El resultado es un array de documentos de la colección task dentro de cada documento de emergencyTask.
+Etapa de unwind: Desestructura el array de documentos de task creado por la etapa anterior, generando un documento por cada tarea dentro de cada documento de emergencyTask.
+Etapa de match: Filtra los documentos de task restantes por id_state igual a 2 (suponiendo que 2 representa el estado "activo").
+Ejecuta la agregación en la colección emergencyTask utilizando la lista pipeline y asigna el resultado a una variable result.
+Convierte el resultado de la agregación a una lista de objetos TaskMongo y la devuelve.
+En resumen, esta función realiza una consulta compleja en MongoDB para recuperar una lista de tareas activas para una emergencia específica, utilizando etapas de agregación para filtrar, unir y procesar los datos de las colecciones involucradas.
+     */
+
+
     @Override
     public List<TaskMongo> getActiveTasksForEmergency(long emergencyId) {
         MongoCollection<TaskMongo> taskCollection = database.getCollection("task", TaskMongo.class);
